158ba1b3775e4d326d3a6fdcb56b7662c67b4e2b
Justin Ross <jross@apache.org>
2021-06-14 07:48:43 -0400

    Try this!

--- client/src/main/java/net/example/Client.java
+++ client/src/main/java/net/example/Client.java
@@ -5,31 +5,24 @@ import io.quarkus.runtime.annotations.QuarkusMain;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 import javax.enterprise.context.ApplicationScoped;
-import javax.inject.Inject;
 import org.eclipse.microprofile.reactive.messaging.Channel;
 import org.eclipse.microprofile.reactive.messaging.Emitter;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
 
-@ApplicationScoped
 @QuarkusMain
 public class Client implements QuarkusApplication {
-    @Inject
+    static int desired = 10;
+    static CountDownLatch completion = new CountDownLatch(desired);
+
     @Channel("outgoing-messages")
     Emitter<String> emitter;
 
-    int desired = 10;
-    CountDownLatch completion = new CountDownLatch(desired);
-
     @Override
-    public int run(String... args) throws Exception {
-        java.lang.Thread.sleep(3000);
-
+    public int run(String... args) {
         try {
             for (int i = 1; i <= desired; i++) {
                 String message = "message " + i;
-
                 emitter.send(message);
-
                 System.out.println("Sent " + message);
             }
 
@@ -50,9 +43,12 @@ public class Client implements QuarkusApplication {
         return 0;
     }
 
-    @Incoming("incoming-messages")
-    public void receive(String message) {
-        System.out.println("Received " + message);
-        completion.countDown();
+    @ApplicationScoped
+    public static class Receiver {
+        @Incoming("incoming-messages")
+        public void receive(String message) {
+            System.out.println("Received " + message);
+            completion.countDown();
+        }
     }
 }
